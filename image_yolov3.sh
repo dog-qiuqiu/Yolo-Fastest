ab429cbacb1bec45a093e2b50a8ccdfe99f865b3
qiuqiu <qiuqiu@example.com>
2020-09-10 00:33:03 +0800

    commit

--- image_yolov3.sh
+++ image_yolov3.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector  test ./data/voc.data  ./Yolo-Fastest/yolo-fastest.cfg  ./Yolo-Fastest/yolo-fastest.weights  data/dog.jpg  -i 0 -thresh 0.25
+./darknet detector  test ./data/voc.data  ./Yolo-Fastest/VOC/yolo-fastest.cfg  ./Yolo-Fastest/VOC/yolo-fastest.weights  data/dog.jpg  -i 0 -thresh 0.25
 
 
 

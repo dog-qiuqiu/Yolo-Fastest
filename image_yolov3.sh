eb4137ca0179d00c1d5edeca193643332ca93071
dog-qiuqiu <67037892+dog-qiuqiu@users.noreply.github.com>
2020-09-10 16:07:31 +0800

    Update image_yolov3.sh

--- image_yolov3.sh
+++ image_yolov3.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector  test ./data/voc.data  ./Yolo-Fastest/VOC/yolo-fastest-xl.cfg  ./Yolo-Fastest/VOC/yolo-fastest-xl.weights  data/dog.jpg  -i 0 -thresh 0.25
+./darknet detector  test ./data/voc.data  ./Yolo-Fastest/VOC/yolo-fastest-xl.cfg  ./Yolo-Fastest/VOC/yolo-fastest-xl.weights  data/dog.jpg  -thresh 0.55
 
 
 

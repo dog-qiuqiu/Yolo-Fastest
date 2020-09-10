2c8b56de0464e62dc19b6b423c4ce7736d4071e0
dog-qiuqiu <67037892+dog-qiuqiu@users.noreply.github.com>
2020-09-10 16:06:04 +0800

    Update video_yolov3.sh

--- video_yolov3.sh
+++ video_yolov3.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector  demo ./data/voc.data  ./Yolo-Fastest/VOC/yolo-fastest.cfg  ./Yolo-Fastest/VOC/yolo-fastest.weights  data/traffic2_2.mp4  -i 0 -thresh 0.25
+./darknet detector  demo ./data/voc.data  ./Yolo-Fastest/VOC/yolo-fastest-xl.cfg  ./Yolo-Fastest/VOC/yolo-fastest-xl.weights  data/traffic2_2.mp4  -i 0 -thresh 0.25
 
 
 
